a23647b94bf6c25d131c3608538b5a9565b03efa
MD-Tyas <a393b4ky3108@bangkit.academy>
2024-11-18 08:05:43 +0700

    Menambah file kelas EmailInputText

---
+++ app/src/main/java/com/tyas/smartfarm/view/pages/customview/EmailInputText.kt
@@ -0,0 +1,33 @@
+package com.tyas.smartfarm.view.pages.customview
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import com.google.android.material.textfield.TextInputEditText
+import com.tyas.smartfarm.R
+
+class EmailInputText @JvmOverloads constructor(
+   context: Context,
+   attrs: AttributeSet? = null
+) : TextInputEditText(context, attrs) {
+
+   init {
+      addTextChangedListener(object : TextWatcher {
+         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+         override fun afterTextChanged(s: Editable?) {
+            validateEmail(s.toString())
+         }
+      })
+   }
+
+   private fun validateEmail(email: String) {
+      error = if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+         context.getString(R.string.format_email)
+      } else {
+         null
+      }
+   }
+}
